020c5b024049323d610b1bebdce35da3c84190cc
sabir ullah <sabir1khan123@gmail.com>
2020-11-20 14:59:47 +0000

    data js

---
+++ data.js
@@ -0,0 +1,45 @@
+var data = [
+    {
+        "subject": "French",
+        "location": "Paris",
+        "price": 13,
+        "available": 87,
+        "teacher": "John Doe",
+        "fa": "fa-language"
+    },
+    {
+        "subject": "English",
+        "location": "NY",
+        "price": 21,
+        "available": 12,
+        "fa": "fa-language"
+    },
+    {
+        "subject": "Math",
+        "location": "London",
+        "price": 13,
+        "available": 68,
+        "fa": "fa-calculator"
+    },
+    {
+        "subject": "Spanish",
+        "location": "Madrid",
+        "price": 22,
+        "available": 0,
+        "fa": "fa-language"
+    },
+    {
+        "subject": "Physics",
+        "location": "London",
+        "price": 17,
+        "available": 85,
+        "fa": "fa-medkit"
+    },
+    {
+        "subject": "Finance",
+        "location": "London",
+        "price": 25,
+        "available": 16,
+        "fa": "fa-medkit"
+    }
+];
